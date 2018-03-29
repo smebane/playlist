adb03928c21326815c596f0d8e1fa39625793859
Shelbi Mebane <smebane@cristoreyny.org>
2018-03-29 12:42:27 +0000

    everything added

--- js/script.js
+++ js/script.js
@@ -25,22 +25,22 @@ var mySong = {
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"title":"Walk It Talk It,
+		"artist":"Migos ft Drake",
+		"mp3-url":"https://www.youtube.com/watch?v=A_xWDAbnBSU",
+		"image-url":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FbV4bSXUgFVo%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DbV4bSXUgFVo&docid=SriS-Aki1qYGxM&tbnid=9p7u7D1i_Q7ZHM%3A&vet=10ahUKEwju0L3ExJHaAhVQiqwKHRNVDTUQMwgnKAEwAQ..i&w=1280&h=720&bih=859&biw=1784&q=walk%20it%20talk%20it%20migos&ved=0ahUKEwju0L3ExJHaAhVQiqwKHRNVDTUQMwgnKAEwAQ&iact=mrc&uact=8",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"New Freezer",
+		"artist":"Rich The Kid",
+		"mp3-url":"",
+		"image-url":"",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"Icy Girl",
+		"artist":"Saweetie",
+		"mp3-url":"https://www.youtube.com/watch?v=Wji4b2jjYOk,
+		"image-url":"https://www.google.com/imgres?imgurl=https%3A%2F%2Ft2.genius.com%2Funsafe%2F220x0%2Fhttps%253A%252F%252Fimages.genius.com%252F934310f5acf7439c67f4a8881599b1b2.907x910x1.jpg&imgrefurl=https%3A%2F%2Fgenius.com%2FSaweetie-icy-grl-lyrics&docid=vWQFcmIvA5zYpM&tbnid=cawFYw1UuszCVM%3A&vet=10ahUKEwifvf6AxpHaAhULbKwKHbiKAIYQMwhGKAAwAA..i&w=220&h=221&bih=859&biw=1784&q=icy%20girl&ved=0ahUKEwifvf6AxpHaAhULbKwKHbiKAIYQMwhGKAAwAA&iact=mrc&uact=8",
 	}
 
 ]
